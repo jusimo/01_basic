da8612d027754681f31df061c0cc39187bef1e02
pr08 <pr08@figd-1711708.figdtk.de>
2018-03-12 14:17:10 +0100

    init project 01 basic

---
+++ index.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+    
+
+<html>
+    <head>
+        <title>PHP O1 BASIC</title>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link href="css/bootstrap.min.css" rel="stylesheet" type="text/css"/>
+        <link href="css/styles.css" rel="stylesheet" type="text/css"/>
+        <script src="js/jquery-3.3.1.min.js" type="text/javascript"></script>
+        <script src="js/bootstrap.min.js" type="text/javascript"></script>
+        <script src="js/main.js" type="text/javascript"></script>
+    </head>
+
+    <body>
+        <div class="container">
+            <h1>Überschrift</h1>
+            <label>Name
+                <input type="form-control" type="text">
+            </label>
+
+            <div class="row">
+                <div class="col-sm-12 col-md-6"><h3>Text 1</h3>Alles. PHP ist hauptsächlich auf serverseitige Skripte fokussiert, weshalb Sie alles tun können, was auch ein anderes CGI-Programm kann, wie z.B. Formulardaten sammeln, dynamische Inhalte für Webseiten generieren oder Cookies senden und empfangen. Aber PHP kann noch viel mehr.</div>
+                <div class="col-sm-12 col-md-6"><h3>Text 2</h3>Alles. PHP ist hauptsächlich auf serverseitige Skripte fokussiert, weshalb Sie alles tun können, was auch ein anderes CGI-Programm kann, wie z.B. Formulardaten sammeln, dynamische Inhalte für Webseiten generieren oder Cookies senden und empfangen. Aber PHP kann noch viel mehr.</div>
+            </div>
+        
+        
+        
+            
+        
+        
+        
+        </div>
+        
+        
+    </body>
+   
+</html>
